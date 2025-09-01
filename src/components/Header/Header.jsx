42936a3f238072f5c620804776980a0e50cbc0f9
JustDondo <162633910+CodeDondo@users.noreply.github.com>
2025-09-01 09:44:10 +0200

    Added Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,28 @@
+import Logo from '../../assets/Logo.png'
+import Mail from '../../assets/Important-Mail.png'
+import Info from '../../assets/Info-Squared.png'
+import Account from '../../assets/Test-Account.png'
+import { Link } from 'react-router-dom'
+
+
 
 export const Header = () => {
   return (
     <header>
-        Logo
+      <ul>
+        <img src={Logo} alt="Logo" />
+      </ul>
+      <ul>
+        Kategori
+      </ul>
+      <ul>
+        <button>Opret annonce</button>
+      </ul>
+      <ul>
+        <Link><img src={Mail} alt="Mail" /></Link>
+        <Link><img src={Info} alt="Info" /></Link>
+        <Link><img src={Account} alt="Account" /></Link>
+      </ul>
     </header>
   )
 }
