04254ee9a2dfa8bff2179e1d575e04927eee71cf
JustDondo <162633910+CodeDondo@users.noreply.github.com>
2025-09-02 12:41:51 +0200

    added donos

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -9,22 +9,25 @@ export const CategoryList = () => {
     if (loading) return <div>loading...</div>
     if (error) return <div>{error}</div>
     return (
-        <ul className={styles.grid}>
-            {CategoryList.slice(0, 6).map(Category => {
-                const { id, name, image, slug } = Category
-                return (
-                    <li key={id} className={styles.Category}>
-                        <Link to={`${slug}`} className={styles.link}>
-                            <div className={styles.imageWrapper}>
-                                <img src={image} alt={name} className={styles.image} />
-                                <div className={styles.overlay}>
-                                    <span>{name}</span>
+        <>
+        <h3>Populære kategorier:</h3>
+            <ul className={styles.grid}>
+                {CategoryList.slice(0, 6).map(Category => {
+                    const { id, name, image, slug } = Category
+                    return (
+                        <li key={id} className={styles.Category}>
+                            <Link to={`${slug}`} className={styles.link}>
+                                <div className={styles.imageWrapper}>
+                                    <img src={image} alt={name} className={styles.image} />
+                                    <div className={styles.overlay}>
+                                        <span>{name}</span>
+                                    </div>
                                 </div>
-                            </div>
-                        </Link>
-                    </li>
-                )
-            })}
-        </ul>
+                            </Link>
+                        </li>
+                    )
+                })}
+            </ul>
+        </>
     )
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,10 @@
 import MainLogo from '../../assets/MissionStatement.png'
 import { ItemList } from '../ItemList/ItemList'
 import { CategoryList } from '../CategoryList/CategoryList'
-import { MissionStatement, MainContainer } from './MainStyles'
+import { MissionStatement, MainContainer, ImageWrapper } from './MainStyles'
 import Seperator from '../../assets/Seperator.png'
+import AllTimeDono from '../../assets/AllTime-Banner.png'
+import Y2D from '../../assets/Y2D-Banner.png'
 
 export const Main = () => {
   return (
@@ -17,6 +19,10 @@ export const Main = () => {
         <img src={Seperator} alt="seperator" />
           <CategoryList />
           <img src={Seperator} alt="seperator" />
+          <ImageWrapper>
+            <img src={AllTimeDono} alt="AllTime" />
+            <img src={Y2D} alt="Y2D" />
+          </ImageWrapper>
       </MainContainer>
     </>
   )
